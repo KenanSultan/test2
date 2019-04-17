31f535b999001317e2bd278c29d183ea566d6a2a
KenanSultan <kenansultan@hotmail.com>
2019-04-17 13:18:16 +0400

    test2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,12 @@ $.ajax({
         key: "5OVwGm0ylIwc1aSv8PamQ",
         q: "Improbable"
     },
-    method: "GET"
+    method: "GET",
+    dataType: "xml",
+    success: function(xml){
+        var data = $('doctor',xml).text()
+        alert(data)
+    }
 }).then(function(resp) {
     console.log(resp)
 })
